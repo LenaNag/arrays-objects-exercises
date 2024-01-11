45993ab27f88d988628c294aa83a13c591ed8426
Alena Nahornaya <nahornaya.alena@gmail.com>
2024-01-11 22:54:10 +0100

    zadanie 2

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ function mergeArrays(array1, array2) {
 };
 
 console.log(mergeArrays([2, 5], [3, 7]));
+
+//zadanie 2
+const exceptFirst = (...parameters) => {
+    return parameters.slice(1);
+};
+
+console.log(exceptFirst(2, "delfin", null));
